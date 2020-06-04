20d4f37d83f506727f0e07852eab85bb41e458bf
NathanFS <backentrancehacker@gmail.com>
2020-06-04 08:07:36 +0000

    website

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express'),
 	graphqlHTTP = require('express-graphql'),
 	{buildSchema} = require('graphql')
 
+const path = require('path')
+
 const schema = buildSchema(`
 	type Query {
 		hello: String
@@ -15,11 +17,21 @@ const root = {
 }
 
 const app = express()
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+	res.send('Hello')
+})
+app.all('/simply_clean.css', (req, res) => {
+	res.sendFile(path.join(__dirname, '/node_modules/simply_clean.css/style.css'))
+})
 app.use('/graphql', graphqlHTTP({
 	schema,
 	rootValue: root,
 	graphiql: true
 }))
+
+
 app.listen(4000)
 
 console.log('Running graphql server on port 4000')
